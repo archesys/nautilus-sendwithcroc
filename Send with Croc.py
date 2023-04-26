f77d20808c50f07fb52fd1dcbbefb4613871b742
Archesys <archesys@gmail.com>
2023-04-26 18:23:25 +0200

    New version
    This script version determines automatically the path to the croc software wich can be different depending on your Linux version..

--- Send with Croc.py
+++ Send with Croc.py
@@ -3,22 +3,23 @@ import os
 import pty
 import subprocess
 
-# Path to the Croc executable "/bin/croc" for Manjaro and "/usr/local/bin/croc" for Ubuntu
+""" Path to croc "/bin/croc" for Manjaro and "/usr/local/bin/croc" for Ubuntu """
 CROC_PATH = "/bin/croc"
+if os.path.exists(CROC_PATH):
+    """ don't touch the path.. maybe Manjaro """
+else:
+    """ We modify path.. maybe Ubuntu """
+    CROC_PATH = "/usr/local/bin/croc"
 
 def get_selected_files():
-    """
-    Returns the list of absolute paths of selected files in Nautilus.
-    """
-    files = os.environ.get("NAUTILUS_SCRIPT_SELECTED_FILE_PATHS")
+    """ Returns the list of absolute paths of selected files in Nemo. """
+    files = os.environ.get("NEMO_SCRIPT_SELECTED_FILE_PATHS")
     if files is None:
         return None
     return files.split(";;")
 
 def send_files_with_croc(files):
-    """
-    Sends files with Croc.
-    """
+    """ Sends files with Croc. """
     cmd = [CROC_PATH, "send"] + files
     subprocess.Popen(["gnome-terminal", "--", "bash", "-c", " ".join(cmd)])
 
